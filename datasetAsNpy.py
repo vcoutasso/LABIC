65ffb94912cacc0eb50207608bb59bc076dffaf4
Vinicius Couto <vinicouto12@gmail.com>
2019-09-17 16:54:26 -0300

    Script to read datasets and save both test and train sets to npy files

---
+++ datasetAsNpy.py
@@ -0,0 +1,42 @@
+# Cria dois conjuntos de arquivos npy: train e test. Cada um composto por seu correspondente X, contendo os caminhos para as imagens
+# a serem lidas e y com suas respectivas labels.
+
+import os
+import numpy as np
+import argparse
+
+
+def readDataset(path):
+    X = []
+    y = []
+
+    folders = os.listdir(path)
+    folders.sort()
+
+    for f in folders:
+        for file in os.listdir(''.join((path, '/', f))):
+            X.append(file)
+            y.append(f)
+
+    return np.array(X), np.array(y)
+
+parser = argparse.ArgumentParser(description='Reads images from PATH and stores data as numpy arrays. PATH must contain folders \
+    \'test\' and \'train\'. Inside these folders, data must be organized in subfolders that will name its content class.')
+
+parser.add_argument('-p', '--path', metavar='PATH', required='True', help='Path to read files from.')
+
+args = parser.parse_args()
+
+PATH = args.path
+
+train_path = PATH + '/train'
+test_path = PATH + '/test'
+
+X_train, y_train = readDataset(train_path)
+X_test, y_test = readDataset(test_path)
+
+np.save('X_train.npy', X_train)
+np.save('y_train.npy', y_train)
+
+np.save('X_test.npy', X_test)
+np.save('y_test.npy', y_test)
